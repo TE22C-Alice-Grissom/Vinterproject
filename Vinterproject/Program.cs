dc334e6ee3ad0ed1f30efb29157d34104d7bb181
Alice Grissom <Alice.grissom@elev.ga.ntig.se>
2023-11-20 09:19:57 +0100

    första 15/11-23

--- Vinterproject/Program.cs
+++ Vinterproject/Program.cs
@@ -1,11 +1,32 @@
-﻿
-Rectangle platta = new Rectangle(50)
+﻿using System.Net.Sockets;
+using System.Numerics;
+using Raylib_cs;
+
+Raylib.InitWindow(800, 600, "Breakout");
+Raylib.SetTargetFPS(60);
+
+while (!Raylib.WindowShouldClose())
+{
+    Raylib.BeginDrawing();
+    Raylib.ClearBackground(Color.BLACK);
+
+    Rectangle platta = new Rectangle(10, 10, 50, 20);
+
+    Raylib.EndDrawing();
+}
+
+
+//Raylib.DrawCirle(100, 100, 100, Color.White);
+
+
+
+
+
+//List<Rectangle> rects = new List<Rectangle>();
 
 
 
 
 
 
-
-
-Console.ReadLine();
+// Console.ReadLine();
