c67bcbcb1ebed556ae24255e54bd331d1b25e5f5
Alice Grissom <Alice.grissom@elev.ga.ntig.se>
2023-11-27 09:29:38 +0100

    euewåfibs

--- Vinterproject/Program.cs
+++ Vinterproject/Program.cs
@@ -1,24 +1,66 @@
 ﻿using System.Net.Sockets;
 using System.Numerics;
+using System.Security.Cryptography;
 using Raylib_cs;
 
 Raylib.InitWindow(800, 600, "Breakout");
 Raylib.SetTargetFPS(60);
 
-Color ljusBlå = new Color(0, 191, 255, 1); 
 
 
 
+Color ljusBlå = new Color(0, 191, 255, 255);
+Rectangle plattaRec = new Rectangle(400 - 75, 350, 150, 20);
+
+Vector2 boll = new Vector2(400, 335);
+
+
+Vector2 movement = new Vector2(0, 0);
+
+
+List<Rectangle> block = new();
+
+
+float speed = 5;
+
+string scene = "game";
 
 while (!Raylib.WindowShouldClose())
 {
     Raylib.BeginDrawing();
     Raylib.ClearBackground(Color.WHITE);
 
-    Rectangle plattaRec = new Rectangle(10, 10, 50, 20);
+    Raylib.DrawRectangleRec(plattaRec, ljusBlå);
+    Raylib.DrawCircleV(boll, 10, Color.BLACK);
 
     Raylib.EndDrawing();
-    Raylib.WindowSwapBuffers();
+
+    if (scene == "game")
+    {
+        movement = Vector2.Zero;
+
+        if (Raylib.IsKeyDown(KeyboardKey.KEY_UP))
+        {
+            movement.Y = -1;
+        }
+        else if (Raylib.IsKeyDown(KeyboardKey.KEY_DOWN))
+        {
+            movement.Y = 1;
+        }
+
+        if (Raylib.IsKeyDown(KeyboardKey.KEY_LEFT))
+        {
+            movement.X = -1;
+        }
+        else if (Raylib.IsKeyDown(KeyboardKey.KEY_RIGHT))
+        {
+            movement.X = 1;
+        }
+
+        plattaRec.X += movement.X;
+
+    }
+
 
 }
 
